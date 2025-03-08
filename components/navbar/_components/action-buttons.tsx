2c14ff32e4a8159e21a0e54f7f82e7b103b85628
danial137 <danial79fakhrabadi@gmail.com>
2025-03-08 14:23:37 +0330

    updated!

--- components/navbar/_components/action-buttons.tsx
+++ components/navbar/_components/action-buttons.tsx
@@ -1,12 +1,17 @@
 "use client"
 import React,{useState} from "react"
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 import { X } from "lucide-react"
 
 const ActionButton = () => {
   return (
       <div className="pr-2">
-          <div className="items-center justify-centers flex"></div>
+          <div className="items-center justify-center flex">
+              <div className="flex xl:space-x-4">
+                  <Link href="/contact-sales"></Link>
+              </div>
+          </div>
     </div>
   )
 }
