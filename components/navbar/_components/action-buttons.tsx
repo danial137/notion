0070705ec228b57641d95e09015379e2f0e94e89
danial137 <danial79fakhrabadi@gmail.com>
2025-03-09 15:21:13 +0330

    update

--- components/navbar/_components/action-buttons.tsx
+++ components/navbar/_components/action-buttons.tsx
@@ -1,19 +1,35 @@
 "use client"
-import React,{useState} from "react"
+import React, { useState } from "react"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import { X } from "lucide-react"
 
 const ActionButton = () => {
-  return (
-      <div className="pr-2">
-          <div className="items-center justify-center flex">
-              <div className="flex xl:space-x-4">
-                  <Link href="/contact-sales"></Link>
-              </div>
-          </div>
-    </div>
-  )
+    const [isdropdown, setisdropdown] = useState(false)
+    const toggleDropdown = () => setisdropdown(!isdropdown)
+    const closedropdown = () => setisdropdown(!isdropdown)
+    return (
+        <div className="pr-2">
+            <div className="items-center justify-center flex">
+                <div className="flex xl:space-x-4">
+                    <Link href="/contact-sales" className="hidden lg:flex items-center">
+                        <div>request a demo</div>
+                    </Link>
+                    <div className="font-thin lg-flex items-flex hidden">|</div>
+                    <div className="flex lg:space-x-4 items-center pr-4">
+                        <div>
+                            <Button variant={"outline"} className="hidden lg:flex items-center border-none text-md"> LogIn </Button>
+                        </div>
+                    </div>
+                    <div className="flex lg:space-x-4 items-center pr-4">
+                        <div>
+                            <Button  className="hidden lg:flex items-center border-none text-md"> Get Bird  Free </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ActionButton
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,11 +3,20 @@ import Logo from './_components/logo'
 import { Menu } from './_components/menu'
 import ActionButton from './_components/action-buttons'
 const Navbar = () => {
+
+  const navBarClasses = `flex items-center justify-between space-x-10 bg-white h-14 sticky top-0 z-50 border-b border-gray-200`
+
   return (
-      <div className='flex items-center'>
-          <Logo />
-      <Menu />
-      <ActionButton/>
+    <div className={navBarClasses}>
+      <div className='flex items-center justify-center'>
+
+
+        <Logo />
+        <Menu />
+      </div>
+        <ActionButton />
+
+     
     </div>
   )
 }
